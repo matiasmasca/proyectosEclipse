b4bbafaddff1ad714150ce1c909e842933ac472c
unknown <cursoadmin@alumno08.poloitcorrientes.local>
2014-06-30 16:08:48 -0300

    terminando la clase

---
+++ practica/practica6/src/com/cursoAndroid/practica6/MainActivity.java
@@ -0,0 +1,63 @@
+package com.cursoAndroid.practica6;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	private Button mMenu;
+	private Button mMenuContextuales;
+	private Button mSalir;
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		addWidgets();
+		addListeners();
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState ){
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+	}
+
+	private void addListeners() {
+		mMenu.setOnClickListener(this);
+		mMenuContextuales.setOnClickListener(this);
+		mSalir.setOnClickListener(this);
+	}
+
+	private void addWidgets(){
+		mMenu = (Button) findViewById(R.id.main_activity_btnMenus);
+		mMenuContextuales = (Button) findViewById(R.id.main_activity_btnMenuContextual);
+		mSalir = (Button) findViewById(R.id.main_activity_btnSalir);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent();
+    	
+    	switch(v.getId()){
+		 case R.id.main_activity_btnSalir:
+			 finish();
+			 break;
+		 case R.id.main_activity_btnMenus:
+			 intent.setClass(this, MenuActivity.class);
+			 startActivity(intent);
+			 break;
+		case R.id.main_activity_btnMenuContextual:
+			intent.setClass(this, MenuConextualActivity.class);
+			startActivity(intent);
+			break;
+		}
+
+	}
+	
+	
+}
---
+++ practica/practica6/src/com/cursoAndroid/practica6/MenuActivity.java
@@ -0,0 +1,12 @@
+package com.cursoAndroid.practica6;
+
+public class MenuActivity extends Activity {
+	private static final int MNU_OPC1;
+	private static final int MNU_OPC2;
+	private static final int MNU_OPC3;
+  
+	private TextView mMensaje;
+	
+	
+	
+}
